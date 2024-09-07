a373e4822d7586adf82c1d88c83efebea04d7fee
Annapurna Mishra <146976500+Annapurna0311@users.noreply.github.com>
2024-09-07 17:11:40 +0530

    Add files via upload

---
+++ Docker/demoproject/myapp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+const int mod = 1000000007;
+#define ll long long
+#define mod 1000000007
+
+int main() {
+    cout << "perform sum : ";
+    int a , b;
+    cout << "enter no first" ;
+    cin >> a;
+    cout << "enter no second" ;
+    cin >> b;
+    int res = a + b;
+    cout << "the result is sum " << res << endl;
+    return 0;
+}
